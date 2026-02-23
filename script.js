9ba83d8aebbc685821a75455e6767e41653a344e
Aman_Singh <anuragkumar002004@gmail.com>
2026-02-23 22:41:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+
+
+// const display = document.getElementById("display");
+
+
+// function appendToDisplay(input){
+//     display.value += input;
+// }
+
+
+// function clearDisplay(){
+//    display.value = "";
+// }
+
+// function calculate(){
+
+//     try {
+//         display.value = eval(display.value);
+//     } catch (error) {
+//         display.value = "Error";
+//     }
+// }
+
+
+
+let display = document.getElementById("display");
+
+function append(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function del() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
